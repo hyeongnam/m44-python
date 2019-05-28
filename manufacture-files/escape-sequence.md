3c5806ccac268f43e63aa0fc79fa5e9d9b7c4ba0
hyeongnam <morefly@naver.com>
2019-05-28 15:39:26 +0900

    Create text-ex app

---
+++ manufacture-files/escape-sequence.md
@@ -0,0 +1,9 @@
+# Escape sequence
+
+- `\n` : LF(Line Feed) -> Unix
+- `\r` : CR(Carriage Return) -> Mac OS before X
+- `\r\n` : CR + LF -> Windows
+- `\t` : 탭 문자
+- `\\` : \(백슬래쉬)를 문자로 인식
+- `\'`, `\"` : 따옴표
+ 
---
+++ manufacture-files/text-ex/reverse.py
@@ -0,0 +1,13 @@
+
+with open('numbers.txt','r') as f:
+    lines = f.readlines()
+# 2.list 뒤집기
+lines.reverse()
+
+with open('numbers.txt','w') as f:
+    for line in lines:
+        # String.strip() 문자열의 처음과 끝의 공백을 지워주는 메서드
+        f.write(line.rstrip() + '\n') # 오른쪽 공백만 지워라 => rstrip()
+
+
+
